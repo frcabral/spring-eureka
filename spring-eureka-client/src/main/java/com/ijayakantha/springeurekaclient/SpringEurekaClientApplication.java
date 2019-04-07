ecc9187d530b99c25c353f2e7af90b78d1ddd249
Isuru Jay <ijayakantha@virtusa.com>
2019-04-07 16:34:00 +0530

    updated configurations

--- spring-eureka-client/src/main/java/com/ijayakantha/springeurekaclient/SpringEurekaClientApplication.java
+++ spring-eureka-client/src/main/java/com/ijayakantha/springeurekaclient/SpringEurekaClientApplication.java
@@ -1,10 +1,8 @@
 package com.ijayakantha.springeurekaclient;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Lazy;
@@ -12,8 +10,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @EnableDiscoveryClient
 @SpringBootApplication
@@ -27,7 +23,6 @@ public class SpringEurekaClientApplication {
 class EurekaClientRestController {
 
     @Autowired
-    @Lazy
     private DiscoveryClient discoveryClient;
 
     @RequestMapping("/clients/{applicationName}")
