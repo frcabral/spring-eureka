2c406a9f95511c66b8e0dd505b60356dfed1a6a4
Isuru Jay <ijayakantha@virtusa.com>
2019-04-07 14:40:16 +0530

    eureka service client and server updated

---
+++ spring-eureka-client/src/main/java/com/ijayakantha/springeurekaclient/SpringEurekaClientApplication.java
@@ -0,0 +1,37 @@
+package com.ijayakantha.springeurekaclient;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class SpringEurekaClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringEurekaClientApplication.class, args);
+    }
+}
+
+@RestController
+class EurekaClientRestController {
+
+    @Autowired
+    @Lazy
+    private DiscoveryClient discoveryClient;
+
+    @RequestMapping("/clients/{applicationName}")
+    public @ResponseBody String getClientsByApplicationName(@PathVariable String applicationName) {
+        return this.discoveryClient.getInstances(applicationName).get(0).getUri().toString();
+    }
+}
